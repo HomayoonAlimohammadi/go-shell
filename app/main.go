7b16c8de03bd38c578b7ffd985b3283071cb3239
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2025-06-28 13:15:05 +0400

    feat: REPL
    Signed-off-by: Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>

--- app/main.go
+++ app/main.go
@@ -7,15 +7,17 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
+	for {
+		fmt.Print("$ ")
 
-	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Printf("failed to read command: %s", err)
-	}
+		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Printf("failed to read command: %s", err)
+		}
 
-	cmd = cmd[:len(cmd)-1]
-	if err := handleCmd(cmd); err != nil {
-		fmt.Printf("%s: %s", cmd, err)
+		cmd = cmd[:len(cmd)-1]
+		if err := handleCmd(cmd); err != nil {
+			fmt.Printf("%s: %s\n", cmd, err)
+		}
 	}
 }
